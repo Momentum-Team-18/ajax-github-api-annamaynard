9896eef1898eef8befdfbd7e92f38be1867f484c
annamaynard <anna.maynard@gmail.com>
2023-04-27 16:34:36 -0400

    avatarPic & userName

--- main.js
+++ main.js
@@ -1,14 +1,36 @@
 let mainContainer = document.querySelector("#mainContainer");
 console.log(mainContainer);
 
-let gitHubUrl = "https://api.github.com/users/annamaynard";
-console.log(gitHubUrl)
+let gitUrl = "https://api.github.com/users/annamaynard";
+console.log(gitUrl)
 
-fetch(gitHubUrl, {
+fetch(gitUrl, {
     method: "GET",
     headers: { "Content-Type": "application/json" },
 })
 
-.then((response) => { 
+.then((response) => {
     return response.json();
+})
+
+.then((parsedResponse)=> {
+
+console.log(parsedResponse);
+
+// //HTML
+
+let avatarPic = document.createElement("img");
+avatarPic.src = parsedResponse.avatar_url;
+mainContainer.appendChild(avatarPic);
+avatarPic.classList.add("avatarPic");
+
+let userName = document.createElement('h1');
+userName.innerText = parsedResponse.name;
+mainContainer.appendChild(userName);
+userName.classList.add("userName");
+
+// // let location = document.createElement("h2");
+// // userName.innerText = `${data.location} \n GitHub URL:`;
+// // location.appendChild("div");
+
 })
