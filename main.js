c515148cf2d3ace5636c50c3e86ba4c6bec93bf2
annamaynard <anna.maynard@gmail.com>
2023-04-27 20:09:25 -0400

    added info & styling attempt

--- main.js
+++ main.js
@@ -1,7 +1,16 @@
-let mainContainer = document.querySelector("#mainContainer");
-console.log(mainContainer);
+// let mainContainer = document.querySelector("#mainContainer");
+// let photoName= document.querySelector("#photoName");
+// let userInfo= document.querySelector("#userInfo");
+// let userRepos = document.querySelector("#userRepos");
+
+// ^trying to figure out how to style this so that they are in seperate <divs> on the browser. ??? 
+
+
+
 
 let gitUrl = "https://api.github.com/users/annamaynard";
+
+
 console.log(gitUrl)
 
 fetch(gitUrl, {
@@ -17,20 +26,63 @@ fetch(gitUrl, {
 
 console.log(parsedResponse);
 
+
+
 // //HTML
 
+
+        // AVATAR PIC
 let avatarPic = document.createElement("img");
 avatarPic.src = parsedResponse.avatar_url;
-mainContainer.appendChild(avatarPic);
-avatarPic.classList.add("avatarPic");
+avatarPic.classList.add("image");
 
+        // ANNA MAYNARD 
 let userName = document.createElement('h1');
 userName.innerText = parsedResponse.name;
-mainContainer.appendChild(userName);
 userName.classList.add("userName");
 
-// // let location = document.createElement("h2");
-// // userName.innerText = `${data.location} \n GitHub URL:`;
-// // location.appendChild("div");
+
+
+mainContainer.appendChild(avatarPic);
+mainContainer.appendChild(userName);
+
+
+
+// FIRST DIV 
+    // LOCATION 
+    // URL --- make short link  -- why is it not taking on userInfo class? 
+    // GITHUB USERNAME 
+
+
+          // LOCATION
+let userLocation = document.createElement('p');
+userLocation.innerText = `Location :  ${parsedResponse.location}`
+mainContainer.appendChild(userLocation);
+userLocation.classList = ("userInfo");
+ 
+console.log(userLocation)
+        
+        // URL
+let gitHubUrl = document.createElement('p');
+gitHubUrl.innerText = `GitHub URL : ${parsedResponse.url}`;
+mainContainer.appendChild(gitHubUrl);
+userLocation.classList = ("userInfo");
+
+console.log(gitHubUrl);
+
+        // USERNAME
+let userLogin = document.createElement('p');
+userLogin.innerText = `GitHub username : ${parsedResponse.login}`;
+mainContainer.appendChild(userLogin);
+userLogin.classList = ("userInfo");
+
+console.log(userLogin)
+
+
+        // REPOS
+let gitRepos = document.createElement('div');
+gitRepos.innerText = parsedResponse.repos_url; 
+mainContainer.appendChild(gitRepos)
+gitRepos.classList.add=("gitRepos");
 
 })
